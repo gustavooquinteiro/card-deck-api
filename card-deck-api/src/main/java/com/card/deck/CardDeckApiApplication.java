ff6d306fd71cac18d635208772c90bfc0707e44a
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-09 11:19:57 -0300

    add CardDeckApiApplication class

---
+++ card-deck-api/src/main/java/com/card/deck/CardDeckApiApplication.java
@@ -0,0 +1,13 @@
+package com.card.deck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardDeckApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardDeckApiApplication.class, args);
+	}
+
+}
