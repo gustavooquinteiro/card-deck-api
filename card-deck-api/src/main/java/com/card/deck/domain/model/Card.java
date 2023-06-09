75c45b64a1deaba1db3da168d11c177e35c140fb
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-09 11:18:33 -0300

    add Card class

---
+++ card-deck-api/src/main/java/com/card/deck/domain/model/Card.java
@@ -0,0 +1,38 @@
+package com.card.deck.domain.model;
+
+public class Card {
+
+	private static int ACE_VALUE = 1;
+	private static int JESTER_VALUE = 11;
+	private static int QUEEN_VALUE = 12;
+	private static int KING_VALUE = 13;
+	
+	private String suit;
+	private String rank;
+	
+	public int getValue() {
+		return switch (getRank()) { 
+		case "A" -> ACE_VALUE;
+		case "J" -> JESTER_VALUE;
+		case "Q" -> QUEEN_VALUE;
+		case "K" -> KING_VALUE;
+		default -> Integer.parseInt(getRank());
+		};
+	}
+	
+	public String getSuit() {
+		return this.suit;
+	}
+	
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	
+	public String getRank() {
+		return this.rank;
+	}
+	
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+}
