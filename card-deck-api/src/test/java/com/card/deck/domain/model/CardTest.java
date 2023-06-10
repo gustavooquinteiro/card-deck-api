efec3097d1d1489a24f1831ca72242f6043376e3
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-10 12:35:48 -0300

    refactor test to use correct static variable and fix range of numerical cards

--- card-deck-api/src/test/java/com/card/deck/domain/model/CardTest.java
+++ card-deck-api/src/test/java/com/card/deck/domain/model/CardTest.java
@@ -16,14 +16,14 @@ public class CardTest {
 		Card kingCard = new Card("Hearts", "K");
 		
 		assertEquals(Card.ACE_VALUE, aceCard.getValue());
-		assertEquals(Card.JESTER_VALUE, jesterCard.getValue());
+		assertEquals(Card.JACK_VALUE, jesterCard.getValue());
 		assertEquals(Card.QUEEN_VALUE, queenCard.getValue());
 		assertEquals(Card.KING_VALUE, kingCard.getValue());
 	}
 	
 	@Test
 	void shouldReturnCorrectCardValue_whenNumericalRankCardValue() {
-		for (int cardValue = 1; cardValue <= 10; cardValue++) {
+		for (int cardValue = 2; cardValue <= 10; cardValue++) {
 			Card card = new Card("Hearts", String.valueOf(cardValue));
 			assertEquals(cardValue, card.getValue());
 		}
