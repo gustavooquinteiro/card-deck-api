165dbfecae441d0974755d6252b53f8015f04fe9
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-11 12:15:32 -0300

    add service to connect to external card API

---
+++ card-deck-api/src/main/java/com/card/deck/domain/service/ExternalCardApiService.java
@@ -0,0 +1,36 @@
+package com.card.deck.domain.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.card.deck.api.dto.DeckResponseExternalApiDTO;
+import com.card.deck.domain.model.Card;
+import com.card.deck.domain.model.Deck;
+import com.card.deck.domain.model.Hand;
+
+@Service
+public class ExternalCardApiService {
+
+	private static String DECK_OF_CARDS_API_URL = "https://deckofcardsapi.com/api/deck/";
+	private static String NEW_SHUFFLE_URL = "new/shuffle/";
+	private static String DRAW_URL = "/draw/";
+	private static String CARD_QUANTITY_PARAM = "?count=" + Hand.HAND_DEFAULT_SIZE;
+
+	private final RestTemplate deckOfCardsApiTemplate = new RestTemplate();
+
+	public Deck retrieveDeck() {
+		return deckOfCardsApiTemplate.getForObject(DECK_OF_CARDS_API_URL + NEW_SHUFFLE_URL, Deck.class);
+	}
+
+	public List<Card> getCardsFromDeck(String deckId) {
+		String uri = DECK_OF_CARDS_API_URL + deckId + DRAW_URL + CARD_QUANTITY_PARAM;
+		DeckResponseExternalApiDTO cardResponse = deckOfCardsApiTemplate.getForObject(uri, DeckResponseExternalApiDTO.class);
+		return cardResponse.cards().stream()
+				.map(card -> new Card(card.suit(), card.value()))
+				.collect(Collectors.toList());
+	}
+
+}
