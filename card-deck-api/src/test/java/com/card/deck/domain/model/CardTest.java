2240a6bdfba9affd79889352115976f5f07f9a64
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-12 07:15:42 -0300

    refactor tests to use correct class methods

--- card-deck-api/src/test/java/com/card/deck/domain/model/CardTest.java
+++ card-deck-api/src/test/java/com/card/deck/domain/model/CardTest.java
@@ -8,24 +8,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class CardTest {
 
+	private static String ACE_NAME = "ACE";
+	private static String JACK_NAME = "JACK";
+	private static String QUEEN_NAME = "QUEEN";
+	private static String KING_NAME = "KING";
+	private static String SUIT = "Hearts";
+	
 	@Test
 	void shouldReturnCorrectCardValue_whenNonNumericalRankCardValue() {
-		Card aceCard = new Card("Hearts", "A");
-		Card jesterCard = new Card("Hearts", "J");
-		Card queenCard = new Card("Hearts", "Q");
-		Card kingCard = new Card("Hearts", "K");
+		Card aceCard = new Card(SUIT, ACE_NAME);
+		Card jesterCard = new Card(SUIT, JACK_NAME);
+		Card queenCard = new Card(SUIT, QUEEN_NAME);
+		Card kingCard = new Card(SUIT, KING_NAME);
 		
-		assertEquals(Card.ACE_VALUE, aceCard.getValue());
-		assertEquals(Card.JACK_VALUE, jesterCard.getValue());
-		assertEquals(Card.QUEEN_VALUE, queenCard.getValue());
-		assertEquals(Card.KING_VALUE, kingCard.getValue());
+		assertEquals(Card.ACE_VALUE, aceCard.getIntValue());
+		assertEquals(Card.JACK_VALUE, jesterCard.getIntValue());
+		assertEquals(Card.QUEEN_VALUE, queenCard.getIntValue());
+		assertEquals(Card.KING_VALUE, kingCard.getIntValue());
 	}
 	
 	@Test
 	void shouldReturnCorrectCardValue_whenNumericalRankCardValue() {
 		for (int cardValue = 2; cardValue <= 10; cardValue++) {
-			Card card = new Card("Hearts", String.valueOf(cardValue));
-			assertEquals(cardValue, card.getValue());
+			Card card = new Card(SUIT, String.valueOf(cardValue));
+			assertEquals(cardValue, card.getIntValue());
 		}
 	}
 }
