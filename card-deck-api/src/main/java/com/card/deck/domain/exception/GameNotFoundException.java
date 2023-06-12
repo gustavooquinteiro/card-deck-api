022b934defb1f03820af71fa782bc479031a6c21
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-12 14:21:02 -0300

    rename exception to be more generic

--- card-deck-api/src/main/java/com/card/deck/domain/exception/GameNotFoundException.java
+++ card-deck-api/src/main/java/com/card/deck/domain/exception/GameNotFoundException.java
@@ -4,15 +4,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class GameDeckNotFoundException extends CardGameException{
+public class GameNotFoundException extends CardGameException{
 
 	private static final long serialVersionUID = 1L;
 	
-	public GameDeckNotFoundException(String message) {
+	public GameNotFoundException(String message) {
 		super(message);
 	}
 	
-	public GameDeckNotFoundException(String message, Throwable cause) {
+	public GameNotFoundException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
