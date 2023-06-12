ce984812fec45ede4c8fad07f35be4ea9f3b969e
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-12 09:57:05 -0300

    refactor to let API's user pass the card quantity

--- card-deck-api/src/main/java/com/card/deck/domain/service/ExternalCardApiService.java
+++ card-deck-api/src/main/java/com/card/deck/domain/service/ExternalCardApiService.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 import com.card.deck.api.dto.DeckResponseExternalApiDTO;
 import com.card.deck.domain.model.Card;
 import com.card.deck.domain.model.Deck;
-import com.card.deck.domain.model.Hand;
 
 @Service
 public class ExternalCardApiService {
@@ -17,7 +16,7 @@ public class ExternalCardApiService {
 	private static String DECK_OF_CARDS_API_URL = "https://deckofcardsapi.com/api/deck/";
 	private static String NEW_SHUFFLE_URL = "new/shuffle/";
 	private static String DRAW_URL = "/draw/";
-	private static String CARD_QUANTITY_PARAM = "?count=" + Hand.HAND_DEFAULT_SIZE;
+	private static String CARD_QUANTITY_PARAM = "?count=";
 
 	private final RestTemplate deckOfCardsApiTemplate = new RestTemplate();
 
@@ -25,8 +24,8 @@ public class ExternalCardApiService {
 		return deckOfCardsApiTemplate.getForObject(DECK_OF_CARDS_API_URL + NEW_SHUFFLE_URL, Deck.class);
 	}
 
-	public List<Card> getCardsFromDeck(String deckId) {
-		String uri = DECK_OF_CARDS_API_URL + deckId + DRAW_URL + CARD_QUANTITY_PARAM;
+	public List<Card> getCardsFromDeck(String deckId, int cardsQuantity) {
+		String uri = DECK_OF_CARDS_API_URL + deckId + DRAW_URL + CARD_QUANTITY_PARAM + cardsQuantity;
 		DeckResponseExternalApiDTO cardResponse = deckOfCardsApiTemplate.getForObject(uri, DeckResponseExternalApiDTO.class);
 		return cardResponse.cards().stream()
 				.map(card -> new Card(card.suit(), card.value()))
