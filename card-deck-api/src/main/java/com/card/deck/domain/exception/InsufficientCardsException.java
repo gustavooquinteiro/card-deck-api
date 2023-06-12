154140a5f21bac958bc488faa9daab447f28c0e5
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-12 11:13:32 -0300

    add InsufficientCardsException class

---
+++ card-deck-api/src/main/java/com/card/deck/domain/exception/InsufficientCardsException.java
@@ -0,0 +1,21 @@
+package com.card.deck.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InsufficientCardsException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static final String DEFAULT_MESSAGE = "A card game should have a positive non-zero number of cards";
+	
+	public InsufficientCardsException() {
+		super(DEFAULT_MESSAGE);
+	}
+	
+	public InsufficientCardsException(Throwable cause) {
+		super(DEFAULT_MESSAGE, cause);
+	}
+
+}
