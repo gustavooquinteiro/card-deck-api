f5dff8f779b2c805e84879841de1f294d13bc6bd
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-13 14:14:41 -0300

    add WinnerDTO property and JsonInclude.Include.NON_NULL annotation

--- card-deck-api/src/main/java/com/card/deck/api/dto/WinnerDTO.java
+++ card-deck-api/src/main/java/com/card/deck/api/dto/WinnerDTO.java
@@ -2,16 +2,21 @@ package com.card.deck.api.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description = "DTO for winner response")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record WinnerDTO(
 		@ApiModelProperty(notes = "Boolean that indicates if the game have or not a winner")
 		boolean hasWinner,
 		@ApiModelProperty(notes = "Boolean that indicates if the game it's a draw or not")
 		boolean isDraw,
-		@ApiModelProperty(notes = "List with the winner, if the game has a winner, or with all the players, if it's a draw")
-		List<PlayerDTO> player) {
+		@ApiModelProperty(notes = "The winner of a game")
+		PlayerDTO winner,
+		@ApiModelProperty(notes = "List with all the players, if the game it's a draw")
+		List<PlayerDTO> players) {
 
 }
