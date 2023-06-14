765f30df68bac44a88eb30637af5f273274bd124
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2023-06-14 09:15:12 -0300

    add validation annotations

--- card-deck-api/src/main/java/com/card/deck/api/dto/CustomGameRequestDTO.java
+++ card-deck-api/src/main/java/com/card/deck/api/dto/CustomGameRequestDTO.java
@@ -2,8 +2,14 @@ package com.card.deck.api.dto;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+
+import com.card.deck.core.validation.SufficientPlayerQuantity;
+
 public record CustomGameRequestDTO(
+		@SufficientPlayerQuantity
 		int player_quantity, 
+		@NotNull
 		List<PlayerDTO> players
 		){
 }
